b172318c4e353f4b753b24449ce855fa67413450
Carlos "Café Café" García <58571285+Carlos-CafeCafe@users.noreply.github.com>
2024-08-22 09:26:46 -0300

    Stop alert added

--- main.ts
+++ main.ts
@@ -17,23 +17,31 @@ input.onButtonPressed(Button.A, function () {
 function consultarBandera () {
     radio.sendValue("consulta", bandera)
 }
+input.onButtonPressed(Button.B, function () {
+    alerta = 0
+})
 radio.onReceivedValue(function (name, value) {
     if (name == "bandera") {
         bandera = value
         basic.showNumber(bandera)
         basic.pause(2000)
         basic.clearScreen()
+        if (bandera == 5) {
+            alerta = 1
+        }
     }
 })
 let bandera = 0
+let alerta = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
+alerta = 0
 consultarBandera()
 basic.showIcon(IconNames.Heart)
 basic.pause(2000)
 basic.clearScreen()
 basic.forever(function () {
-    while (bandera == 5) {
+    while (alerta == 1) {
         basic.showIcon(IconNames.No)
         for (let index = 0; index < bandera; index++) {
             music.play(music.tonePlayable(587, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
